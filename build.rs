af24858879b71cf3881239d7e9d709c095ca29b1
Philip Degarmo <aclysma@gmail.com>
2021-05-26 01:13:26 -0700

    Add a feature to enable a patch to support notify() in the polling crate

--- build.rs
+++ build.rs
@@ -1,7 +1,13 @@
+
 fn main() {
-    cc::Build::new()
-        .file("vendor/wepoll/wepoll.c")
-        .compile("wepoll");
+    let mut build = cc::Build::new();
+    build.file("vendor/wepoll/wepoll.c");
+
+    if cfg!(feature = "null-overlapped-wakeups-patch") {
+        build.define("NULL_OVERLAPPED_WAKEUPS_PATCH", None);
+    }
+
+    build.compile("wepoll");
 
     // We regenerate binding code and check it in. (See generate_bindings.bat)
 }
